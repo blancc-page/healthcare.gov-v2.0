f696943aca4e25f294fc8d445030371570572193
Moses Muta <65918325+blancc-page@users.noreply.github.com>
2025-04-28 20:16:40 +0300

    update[AppContext]: change to production backendUrl

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -7,7 +7,7 @@ export const AppContext = createContext();
 export const AppContextProvider = (props) => {
     axios.defaults.withCredentials = true;
 
-    const backendUrl = import.meta.env.VITE_BACKEND_URL;
+    const backendUrl = 'https://healthcare-gov-backend.onrender.com';
     const [isLoggedIn, setIsLoggedIn] = useState(false);
     const [userData, setUserData] = useState(null);
 
